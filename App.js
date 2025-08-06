72f8394499a4b74d82a77e5d3cc8e9e20ea28c4d
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-08-06 15:58:48 -0700

    3 - Writing React

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Hello world</h1>
+  return (
+    <div>
+      <h1>Hi! World!</h1>
+    </div>
+  );
 }
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+
 import App from "./App";
 
-const container = document.getElementById("root");
-
-/* Edit code below 💖 */
-const root = createRoot(container);
-
-root.render(<App />);
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
